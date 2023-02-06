87b3b34d694c5804f7aca47299eb0196f36149f2
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-02-06 10:15:54 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,4 @@
 import os, torch, timm
-import torchvision.transforms as T
-from PIL import Image
-
-def apply_transformations(im_path, im_size):
-    
-    im = Image.open(im_path)
-    print("Applying transformations...")
-    tfs = T.Compose([T.ToTensor(), T.Resize(im_size), T.RandomHorizontalFlip(),
-                     T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-                     ])
-    
-    return tfs(im)
 
 def get_fm(fm):
         
