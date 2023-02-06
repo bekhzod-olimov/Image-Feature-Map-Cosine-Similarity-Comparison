46d0b42fb0cbb994bf09ea6fe7147b102fd38e12
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-02-06 17:35:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse, yaml
 from utils import *
 from plot import plot_fms
 from transform import apply_transformations
+from PIL import ImageDraw
 
 def run(args):
 
@@ -11,27 +12,29 @@ def run(args):
     cos = torch.nn.CosineSimilarity(dim=1, eps=1e-08)
     model, inp_size = load_model(args.model_name, 1000)
     
-    for i in range(10):
+    for i in range(args.experiments):
     
         im1 = apply_transformations(args.image_path_1, inp_size, False)
         im2 = apply_transformations(args.image_path_1, inp_size, True)
         cos_sim = compute_cos_similarity(model, im1, im2, cos)
         concat_im = preprocess(im1, im2)
-        cv2.imwrite(f"concat_ims/sample_{i}_{cos_sim:.2f}.jpg", concat_im)
-    # values, indices = predict(model, im, args.device)
-    plot_fms(model, im1, f"{args.save_path}/{args.model_name}", args.device, args.model_name)
+        I1 = ImageDraw.Draw(concat_im)
+        I1.text((150, 10), f"Similarity: {cos_sim:.2f}", font=ImageFont.truetype("arial.ttf", 25), fill=(0, 0, 255))
+        os.makedirs(f"concat_ims/{args.model_name}", exist_ok=True)
+        cv2.imwrite(f"concat_ims/{args.model_name}/sample_{i}_{cos_sim:.2f}.jpg", np.array(concat_im))
 
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description='Feature Map Visualization Arguments')
     parser.add_argument('-ip1', '--image_path_1', default='kuvasz.jpg', help='path to the first image')
-    parser.add_argument('-ip2', '--image_path_2', default='cat.jpg', help='path to the second image')
+    parser.add_argument('-ip2', '--image_path_2', default='cup.jpg', help='path to the second image')
     parser.add_argument('-sp', '--save_path', default='fm_images', help='path to a directory to save feature map images')
+    parser.add_argument('-ex', '--experiments', default=5, help='number of experiments')
     parser.add_argument('-d', '--device', default='cuda:3', help='gpu device name')
-    # parser.add_argument('-mn', '--model_name', default='vgg16', help='a model name from timm models')
+    parser.add_argument('-mn', '--model_name', default='vgg16', help='a model name from timm models')
     # parser.add_argument('-mn', '--model_name', default='rexnet_150', help='a model name from timm models')
     # parser.add_argument('-mn', '--model_name', default='darknet53', help='a model name from timm models')
-    parser.add_argument('-mn', '--model_name', default='efficientnet_b3', help='a model name from timm models')
+    # parser.add_argument('-mn', '--model_name', default='efficientnet_b3', help='a model name from timm models')
     
     
     args = parser.parse_args() 
