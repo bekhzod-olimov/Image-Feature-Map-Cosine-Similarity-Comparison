fb60a47f66126abacd8b54c9cd2c14cecc57da9b
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-02-06 16:21:25 +0900

    Update transform.py

--- transform.py
+++ transform.py
@@ -1,12 +1,14 @@
 import torchvision.transforms as T
 from PIL import Image
 
-def apply_transformations(im_path, im_size):
+def apply_transformations(im_path, im_size, random_tfs):
     
     im = Image.open(im_path)
     print("Applying transformations...")
-    tfs = T.Compose([T.ToTensor(), T.Resize(im_size), T.RandomHorizontalFlip(),
+    
+    tfs = T.Compose([T.ToTensor(), T.Resize(im_size), 
                      T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
                      ])
-    
-    return tfs(im)
+    random_rotation = T.Compose([T.RandomRotation(45)])
+   
+    return random_rotation(tfs(im)) if random_tfs else tfs(im)
